a8b19f82de128ba1aa5cacc4599d294d2077d2e9
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-12-08 19:33:15 +0200

    Update trycatch.cpp

--- trycatch.cpp
+++ trycatch.cpp
@@ -3,8 +3,9 @@ void iv_ir_tikr(auto &a) {
   do{
     try {
       
-      if(cin.fail()) {throw std::runtime_error("ivedete netinkama duomeni!!!\n");
-                     }
+      if(cin.fail()) {throw std::runtime_error("ivedete netinkama duomeni!!!\n") }
+      else if (a!=0 || a!=1)  {throw std::runtime_error("ivedete netinkama duomeni!!!\n") }
+   
     }
     catch (const std::runtime_error e) {
       std::cout << e.what () ;
@@ -13,7 +14,7 @@ void iv_ir_tikr(auto &a) {
       cout <<"iveskite duomeni dar karta: ";
       cin>>a;
     }
-  }while (std::cin.fail()==true);
+  }while (std::cin.fail()==true || a!=0 || a!=1);
 }
 void paz_ir_tikr(auto &a) {
   cin>>a;
