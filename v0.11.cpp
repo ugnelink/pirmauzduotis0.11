48be7bbadab43ae5ad22a2ca7b0c9cafd3661a0f
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-12-08 13:17:44 +0200

    Update v0.11.cpp

--- v0.11.cpp
+++ v0.11.cpp
@@ -5,10 +5,11 @@
 #include <iomanip>
 #include <windows.h>
 #include <time.h>
+#include <algorithm>
 using namespace std;
 struct baze{string vardas; string pavarde; float vid; float paz[5]; int kiekis; float egz; float galutinismed; float galutinisvid; float sum=0; float med;}; 
 baze b[1000];
-main() {		
+int main() {		
 long m;
 
 cout <<"iveskite studentu skaiciu: "<< endl;
@@ -25,15 +26,15 @@ cin >> b[i].egz;
 b[i].vid=b[i].sum/b[i].kiekis;
 
 sort (b[i].paz, b[i].paz+b[i].kiekis);
-	if(b[i].kiekis%2==1) b[i].med=b[i].paz[(b[i].kiekis+1)/2]; else b[i].med=( b[i].paz[b[i].kiekis/2]+b[i].paz[(b[i].kiekis/2)+1)/2];
+	if(b[i].kiekis%2==1) b[i].med=b[i].paz[(b[i].kiekis+1)/2]; else b[i].med=b[i].paz[b[i].kiekis/2]+b[i].paz[((b[i].kiekis/2)+1)/2];
   b[i].galutinisvid=0.4*b[i].vid+0.6*b[i].egz;
   b[i].galutinismed=0.4*b[i].med+0.6*b[i].egz;
 }
 cout << endl;
-cout << left << setw(15) << setfill(" ") << "vardas" << left << setw(15) << setfill(" ") << "pavarde" << left << setw(15) << setfill(" ") << "galutinisvid" << left << setw(15) << setfill(" ") << "galutinismed" << endl;
+cout << left << setw(15) << setfill(' ') << "vardas" << left << setw(15) << setfill(' ') << "pavarde" << left << setw(15) << setfill(' ') << "galutinisvid" << left << setw(15) << setfill(' ') << "galutinismed" << endl;
 cout << string(60, '-') << endl; 
 for(long i=1;i<=m;i=i+1) {
-cout << left << setw(15) << setfill(" ") << b[i].vardas << left << setw(15) << setfill(" ") << b[i].pavarde << left << setw(15) << setfill(" ") << fixed << setprecision(2) << b[i].galutinisvid << left << setw(15) << setfill(" ") << fixed << setprecision(2) << b[i].galutinismed << endl;
+cout << left << setw(15) << setfill(' ') << b[i].vardas << left << setw(15) << setfill(' ') << b[i].pavarde << left << setw(15) << setfill(' ') << fixed << setprecision(2) << b[i].galutinisvid << left << setw(15) << setfill(' ') << fixed << setprecision(2) << b[i].galutinismed << endl;
 
 }
 return (0);
