080b74645574bb3ce69f9a76bb59cee1c0947ba4
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-12-08 19:49:47 +0200

    Update trycatch.h

--- trycatch.h
+++ trycatch.h
@@ -3,9 +3,9 @@
 #include <fstream>
 using namespace std;
 
-void iv_ir_tikr(auto &a) ;
-void paz_ir_tikr(auto &a);
-void skaicius_ir_tikr(auto &a);
+void iv_ir_tikr(int &a) ;
+void paz_ir_tikr(float &a);
+void skaicius_ir_tikr(int &a);
 
 
 
