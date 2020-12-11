dbf2c5e23aeb1388a9bdf04990f6b815fd9170a9
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-12-11 15:42:48 +0200

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -8,9 +8,11 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <chrono>
 using namespace std;
 struct baze { string vardas; string pavarde; float vid; vector< int >paz; int kiekis; float egz; float galutinismed; float galutinisvid; float sum = 0; float med; };
 void ketvirta(int irasusk);
-void nuskaitymas(string failas, vector <baze> &studentai);
-void funkcija (string failas, vector <baze> studentai);
-
+void nuskaitymas(string failas, vector <baze>& studentai);
+void funkcija(string failas, vector <baze> studentai);
+void pirma(vector <baze> studentai, vector <baze>& vargsiukai, vector <baze>& galvociai);
+void antra(int irasusk);
