2940dd1d0b3c8c805751ab25a432dc0332e604a2
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-12-11 15:43:37 +0200

    Update trycatch.cpp

--- trycatch.cpp
+++ trycatch.cpp
@@ -1,56 +1,58 @@
 #include "trycatch.h"
-void iv_ir_tikr(int &a) {
-  cin>>a;
-  do{
-    try {
-      
-      if(cin.fail()) {throw std::runtime_error("ivedete netinkama duomeni!!!\n"); }
-      else if (a!=0 || a!=1)  {throw std::runtime_error("ivedete netinkama duomeni!!!\n"); }
-   
-    }
-    catch (const std::runtime_error e) {
-      std::cout << e.what () ;
-      std::cin.clear();
-      std::cin.ignore(256, '\n');
-      cout <<"iveskite duomeni dar karta: ";
-      cin>>a;
-    }
-  }while (std::cin.fail()==true || a!=0 || a!=1);
-}
-void paz_ir_tikr(float &a) {
-  cin>>a;
-  do{
-    try {
-      
-      if(cin.fail()) {throw std::runtime_error("ivedete netinkama duomeni!!!\n");}
-                      else if (a<0) {throw std::runtime_error("ivedete netinkama duomeni!!!\n");}
-        else if  (a>10) {throw std::runtime_error("ivedete netinkama duomeni!!!\n");}          
-    }
-    catch (const std::runtime_error e) {
-      std::cout << e.what () ;
-      std::cin.clear();
-      std::cin.ignore(256, '\n');
-      cout <<"iveskite duomeni dar karta: ";
-      cin>>a;
-    }
-  }while (std::cin.fail()==true ||(a<0) || (a>10) );
-}
-void skaicius_ir_tikr(int &a) {
-  cin>>a;
-  do{
-    try {
-      
-      if(cin.fail()) {throw std::runtime_error("ivedete netinkama duomeni!!!\n");}
-                      else if (a<=0) {throw std::runtime_error("ivedete netinkama duomeni!!!\n");}
-                 
-    }
-    catch (const std::runtime_error e) {
-      std::cout << e.what () ;
-      std::cin.clear();
-      std::cin.ignore(256, '\n');
-      cout <<"iveskite duomeni dar karta: ";
-      cin>>a;
-    }
-  }while (std::cin.fail()==true ||(a<=0) );
-}
+void iv_ir_tikr(int& a) {
+    cin >> a;
+    
+    do {
+        try {
 
+            if (cin.fail()) { throw std::runtime_error("ivedete netinkama duomeni!!!\n");  }
+            else if (a != 0 || a != 1) { throw std::runtime_error("ivedete netinkama duomeni!!!\n"); }
+
+        }
+        catch (const std::runtime_error e) {
+            std::cout << e.what();
+            std::cin.clear();
+            std::cin.ignore(256, '\n');
+            cout << "iveskite duomeni dar karta: ";
+            cin >> a;
+            
+
+        }
+    } while (std::cin.fail() == true || a != 0 || a != 1);
+}
+void paz_ir_tikr(float& a) {
+    cin >> a;
+    do {
+        try {
+
+            if (cin.fail()) { throw std::runtime_error("ivedete netinkama duomeni!!!\n"); }
+            else if (a < 0) { throw std::runtime_error("ivedete netinkama duomeni!!!\n"); }
+            else if (a > 10) { throw std::runtime_error("ivedete netinkama duomeni!!!\n"); }
+        }
+        catch (const std::runtime_error e) {
+            std::cout << e.what();
+            std::cin.clear();
+            std::cin.ignore(256, '\n');
+            cout << "iveskite duomeni dar karta: ";
+            cin >> a;
+        }
+    } while (std::cin.fail() == true || (a < 0) || (a > 10));
+}
+void skaicius_ir_tikr(int& a) {
+    cin >> a;
+    do {
+        try {
+
+            if (cin.fail()) { throw std::runtime_error("ivedete netinkama duomeni!!!\n"); }
+            else if (a <= 0) { throw std::runtime_error("ivedete netinkama duomeni!!!\n"); }
+
+        }
+        catch (const std::runtime_error e) {
+            std::cout << e.what();
+            std::cin.clear();
+            std::cin.ignore(256, '\n');
+            cout << "iveskite duomeni dar karta: ";
+            cin >> a;
+        }
+    } while (std::cin.fail() == true || (a <= 0));
+}
