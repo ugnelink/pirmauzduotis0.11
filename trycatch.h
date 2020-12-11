aaa2a1aa953ffcf073d190e6c03c7beaeca515ad
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-12-11 15:44:07 +0200

    Update trycatch.h

--- trycatch.h
+++ trycatch.h
@@ -1,12 +1,9 @@
+
 #pragma once
 #include <iostream>
 #include <fstream>
 using namespace std;
 
-void iv_ir_tikr(int &a) ;
-void paz_ir_tikr(float &a);
-void skaicius_ir_tikr(int &a);
-
-
-
-
+void iv_ir_tikr(int& a);
+void paz_ir_tikr(float& a);
+void skaicius_ir_tikr(int& a);
