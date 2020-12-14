484a5b2fbcd1b813df0c31fcf1d4083fce57d901
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-12-14 17:28:15 +0200

    Create Header5.h

---
+++ Header5.h
@@ -0,0 +1,26 @@
+#pragma once
+#pragma once
+#include "trycatch.h"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <windows.h>
+#include <time.h>
+#include <algorithm>
+#include <list>
+#include <string>
+#include <chrono>
+using namespace std;
+struct baze { string vardas; string pavarde; float vid; list < int >paz; int kiekis; float egz; float galutinismed; float galutinisvid; float sum = 0; float med; 
+bool operator<(const baze&f) const{
+	if (vardas != f.vardas) return vardas.compare(f.vardas) < 0;
+	if (pavarde != f.pavarde) return pavarde.compare(f.pavarde) < 0;
+	return galutinisvid < f.galutinismed;
+}
+};
+void ketvirta1(int irasusk);
+void nuskaitymas1(string failas, list <baze>& studentai);
+void funkcija1(string failas, list <baze> studentai);
+void pirma1(list <baze> studentai, list <baze>& vargsiukai, list <baze>& galvociai);
+void antra1(int irasusk);
+
