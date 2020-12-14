5f8262965b8fab3f6139dfe0eae3983d3047907a
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-12-14 18:07:57 +0200

    Update Header5.h

--- Header5.h
+++ Header5.h
@@ -11,16 +11,16 @@
 #include <string>
 #include <chrono>
 using namespace std;
-struct baze { string vardas; string pavarde; float vid; list < int >paz; int kiekis; float egz; float galutinismed; float galutinisvid; float sum = 0; float med; 
-bool operator<(const baze&f) const{
+struct baze1 { string vardas; string pavarde; float vid; list < int >paz; int kiekis; float egz; float galutinismed; float galutinisvid; float sum = 0; float med; 
+bool operator<(const baze1&f) const{
 	if (vardas != f.vardas) return vardas.compare(f.vardas) < 0;
 	if (pavarde != f.pavarde) return pavarde.compare(f.pavarde) < 0;
-	return galutinisvid < f.galutinismed;
+	return galutinisvid < f.galutinisvid;
 }
 };
 void ketvirta1(int irasusk);
-void nuskaitymas1(string failas, list <baze>& studentai);
-void funkcija1(string failas, list <baze> studentai);
-void pirma1(list <baze> studentai, list <baze>& vargsiukai, list <baze>& galvociai);
+void nuskaitymas1(string failas, list <baze1>& studentai);
+void funkcija1(string failas, list <baze1> studentai);
+void pirma1(list <baze1> studentai, list <baze1>& vargsiukai, list <baze1>& galvociai);
 void antra1(int irasusk);
-
+ 
